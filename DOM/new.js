05d6e3c9e0a2d9d3c21eec0e5d42160c21171241
officialtalha <31705132+officialtalha>
2023-01-28 19:24:34 +0530

    this is fifth commit

--- DOM/new.js
+++ DOM/new.js
@@ -4,14 +4,9 @@ a.style.border = 'solid 3px black';
 var b = document.getElementById('Additems');
 b.style.fontWeight = 'bold';
 b.style.color = 'green';
-var c = document.getElementsByClassName('list-group-item');
-c[2].style.backgroundColor = 'green';
-for (var i=0; i<c.length; i++)
+var c = document.querySelectorAll('.list-group-item');
+c[1].style.color = 'green';
+for (var i=0; i<c.length; i=i+2)
 {
-    c[i].style.fontWeight = 'bold';
+    c[i].style.backgroundColor = 'green';
 }
-var d = document.getElementsByTagName('li');
-d[4].style.color = 'red';
-
-var e = document.querySelector('.anotherClass');
-e.style.backgroundColor = 'black';
