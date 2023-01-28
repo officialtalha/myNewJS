c4826618b4b19cb6ebd17358a138f42f7280a5ae
officialtalha <31705132+officialtalha>
2023-01-28 17:21:45 +0530

    this is second commit

---
+++ DOM/new.js
@@ -0,0 +1,6 @@
+var a = document.getElementById('header-title');
+a.textContent = 'Talha';
+a.style.border = 'solid 3px black';
+var b = document.getElementById('Additems');
+b.style.fontWeight = 'bold';
+b.style.color = 'green';
