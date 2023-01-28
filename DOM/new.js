f4c28dc98c259a2e6de21b8f49fb5a653d078f05
officialtalha <31705132+officialtalha>
2023-01-28 17:29:56 +0530

    this is third commit

--- DOM/new.js
+++ DOM/new.js
@@ -4,3 +4,9 @@ a.style.border = 'solid 3px black';
 var b = document.getElementById('Additems');
 b.style.fontWeight = 'bold';
 b.style.color = 'green';
+var c = document.getElementsByClassName('list-group-item');
+c[2].style.backgroundColor = 'green';
+for (var i=0; i<c.length; i++)
+{
+    c[i].style.fontWeight = 'bold';
+}
