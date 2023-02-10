45fd28d6028e9e430f85bb4e386bd6ec410f0927
officialtalha <31705132+officialtalha>
2023-02-10 17:40:54 +0530

    final commit

--- DOM/new.js
+++ DOM/new.js
@@ -1,44 +1,57 @@
-//using submit
- var forForm = document.getElementById('form1');
- var forUl = document.getElementById('items');
- 
- forForm.addEventListener('submit', abc);
- forUl.addEventListener('click', xyz);
+var form = document.getElementById('form1');
+var totalItems = document.getElementById('items');
+var filterItems = document.getElementById('filter');
 
- function xyz(f){
-    f.preventDefault();
-    if(f.target.classList.contains('delete'))
-    {
-        if(confirm('Are You Sure?'))
-        {
-            var li = f.target.parentElement;
-            forUl.removeChild(li);
-        }
-    }
- }
+form.addEventListener('submit', addItems);
+totalItems.addEventListener('click', removeItems);
+filterItems.addEventListener('keyup', filterMethod);
 
-
- function abc(e) {
+function addItems(e) {
     e.preventDefault();
 
-    var textSome = document.getElementById('input1').value;
+    var newItem = document.getElementById('input1').value;
+    var newItem2 = document.getElementById('input2').value;
 
-    var newItem = document.createElement('li');
-    newItem.className = 'list-group-item';
-
-    newItem.appendChild(document.createTextNode(textSome));
-
-    var deletebtn = document.createElement('button');
-    deletebtn.className = 'btn btn-danger btn-sm float-right delete';
-    deletebtn.appendChild(document.createTextNode('Delete'));
-
-    var btnedit = document.createElement('button');
-    btnedit.className = 'btn btn-primary btn-sm float-right edit';
-    btnedit.appendChild(document.createTextNode('Edit'));
-
-    newItem.appendChild(btnedit);
-    newItem.appendChild(deletebtn);
-
-    forUl.appendChild(newItem);
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem + ' '));
+    li.appendChild(document.createTextNode(newItem2));
+    totalItems.appendChild(li);
+    //adding edit button
+    var edtBtn = document.createElement('button');
+    edtBtn.className = 'btn btn-primary btn-sm float-right edit';
+    edtBtn.appendChild(document.createTextNode('edit'));
+    li.appendChild(edtBtn);
+    //adding remove button 
+    var dlBtn = document.createElement('button');
+    dlBtn.className = 'btn btn-danger btn-sm float-right delete';
+    dlBtn.appendChild(document.createTextNode('remove'));
+    li.appendChild(dlBtn);
     
+}
+
+function removeItems(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm("Are You Sure?")) {
+            var li = e.target.parentElement;
+            totalItems.removeChild(li);
+        }
+    }
+}
+
+function filterMethod(e){
+    var alpha = e.target.value.toLowerCase();
+    var items = totalItems.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        
+        const itemName = item.childNodes[0].textContent;
+        const description = item.childNodes[1].textContent;
+        
+        if(itemName.toLowerCase().indexOf(alpha) != -1 || description.toLowerCase().indexOf(alpha) != -1){
+            item.style.display = 'block';
+        }
+        else{
+            item.style.display = 'none';
+        }
+    });   
 }
